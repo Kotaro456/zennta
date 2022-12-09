1395991e43731ee7118ca0cbcabac9b5a5ae4b03
Kotaro456 <kotaro03281616@gmail.com>
2022-12-09 21:43:31 +0900

    #22 サイドバー共通化しました。

--- resources/views/user/dashboard/dashboard.blade.php
+++ resources/views/user/dashboard/dashboard.blade.php
@@ -1,21 +1,7 @@
 @extends('app')
 @section('content')
     <div class="flex">
-        <aside
-            class="flex w-80 min-width-sidebar h-full min-h-screen flex-col space-y-4 border-r-2 border-gray-100 bg-white pt-8 px-8">
-            <a href="{{ route('dashboard') }}" class="text-right rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600 bg-purple-500 text-white">
-                記事一覧
-            </a>
-
-            <a href="{{ route('dashboard.following') }}" class="text-right rounded-md pl-auto px-2 py-3 hover:bg-gray-100 hover:text-blue-600">
-                フォローしたアカウント
-            </a>
-
-            <a href="{{ route('dashboard.favorite') }}" class="text-right rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600">
-                いいねした記事
-            </a>
-        </aside>
-
+        @include('user.components.sidebar')
         <div class="w-full p-4">
             <div class="text-black body-font pt-4 pb-16">
                 <div class="container px-5 py-4 mx-auto my-4">
