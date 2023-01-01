9a0cdf44c105022bdd050a6f49cdfdd817a63cc3
Kotaro456 <kotaro03281616@gmail.com>
2023-01-01 18:08:40 +0900

    #26 アプリの日本語設定を行いました。

---
+++ lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'メールアドレスまたはパスワードが間違っています。正しいものを入力してください。',
+    'password' => 'パスワードが間違っています。正しいパスワードを入力してください。',
+    'throttle' => 'ログイン試行上限に達しました。 :seconds 後再度お試しください。',
+
+];
