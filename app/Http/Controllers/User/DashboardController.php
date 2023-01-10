ccafe31f0a663202bdfa0d073633c2aebd9ca3be
Kotaro456 <kotaro03281616@gmail.com>
2023-01-10 21:24:21 +0900

    #32 UserとArticleのリレーションを設定しました。

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
         $user = Auth::user();
-        $articles = Article::where('user_id', $user->id)->get();
+        $articles = $user->articles;
         return view('user.dashboard.dashboard', ['articles' => $articles]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Article;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * ブログポストのコメントを取得
+     * @return HasMany
+     */
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }
