fa974fe2cfd517fff654995a47985920a55b8b33
Kotaro456 <kotaro03281616@gmail.com>
2023-01-23 18:45:29 +0900

    #36 editページのボタンの配置を調整しました。

--- resources/views/user/article/edit.blade.php
+++ resources/views/user/article/edit.blade.php
@@ -16,15 +16,15 @@
             ←
         </a>
 
-        <div class="w-72 flex justify-between">
+        <div class="w-80 flex justify-between">
             <button
                 id="publishOrPrivate"
-                class="publish w-1/4 text-center bg-violet-600 border-0 py-2 px-3 focus:outline-none hover:bg-violet-400 rounded text-base text-white font-semibold mt-4 md:mt-0">
+                class="publish w-36 text-center bg-violet-600 border-0 py-2 px-3 focus:outline-none hover:bg-violet-400 rounded text-base text-white font-semibold mt-4 md:mt-0">
                 公開
             </button>
             <button
                 id="updateArticle"
-                class="w-2/4 text-center bg-violet-600 border-0 py-2 px-3 focus:outline-none hover:bg-violet-400 rounded text-base text-white font-semibold mt-4 md:mt-0">
+                class="w-36 text-center bg-violet-600 border-0 py-2 px-3 focus:outline-none hover:bg-violet-400 rounded text-base text-white font-semibold mt-4 md:mt-0">
                 下書き保存/更新
             </button>
         </div>
